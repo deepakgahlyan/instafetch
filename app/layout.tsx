e26f6744677a8e52a4d1f589c5ddb2cffff5aefe
Deepak Gahlyan <d.gahlyan@gmail.com>
2026-08-11 01:49:17 +0530

    Add Google AdSense verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 const siteUrl = "https://www.instafetch.app";
@@ -57,7 +58,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-zinc-950">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2536087122989226"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
+
+        {children}
+      </body>
     </html>
   );
 }

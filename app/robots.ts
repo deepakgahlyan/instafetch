810aec7be8e3058e692a66178afd448ff9911c01
Deepak Gahlyan <d.gahlyan@gmail.com>
2026-08-11 15:41:44 +0530

    Fix robots sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://instafetch.com";
+const baseUrl = "https://www.instafetch.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
